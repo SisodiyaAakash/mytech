a0066b940dbc06a7c582e2eb676a9b22f77b29c0
Aakash Sakam <aakash@developar.in>
2024-12-11 11:12:31 +0530

    Designed the action area in the header

--- app/comp/header.js
+++ app/comp/header.js
@@ -15,9 +15,43 @@ export default function Header() {
         <input
           type="search"
           placeholder="Search"
-          className="text-sm font-medium outline-0 flex-grow placeholder:text-[#858D9D] "
+          className="text-sm font-medium outline-0 bg-transparent flex-grow placeholder:text-[#858D9D] "
         />
       </div>
+
+      {/* Action Area */}
+      <ul className="flex items-center gap-4">
+        <li className="w-10 h-10 relative flex items-center justify-center">
+          <Image
+            src="/icons/calendar.svg"
+            className="cursor-pointer hover:brightness-0 duration-500"
+            alt="Calendar Icon"
+            width={24}
+            height={24}
+          />
+        </li>
+        <li className="w-10 h-10 relative flex items-center justify-center">
+          <Image
+            src="/icons/bell.svg"
+            className="cursor-pointer hover:brightness-0 duration-500"
+            alt="Calendar Icon"
+            width={24}
+            height={24}
+          />
+        </li>
+        <li className="w-10 h-10 relative flex items-center justify-center">
+          <Image
+            src="/icons/chat.svg"
+            className="cursor-pointer hover:brightness-0 duration-500"
+            alt="Chat Icon"
+            width={24}
+            height={24}
+          />
+        </li>
+        <li className="w-10 h-10 relative flex items-center justify-center">
+          <span className="w-7 h-7 bg-[#E0E2E7] rounded-full"></span>
+        </li>
+      </ul>
     </header>
   );
 }
