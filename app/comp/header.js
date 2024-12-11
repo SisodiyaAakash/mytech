f86231fbe131869e71bdf38a1dc813b7451b562d
Aakash Sakam <aakash@developar.in>
2024-12-11 11:00:05 +0530

    Developed search area

--- app/comp/header.js
+++ app/comp/header.js
@@ -1,7 +1,23 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <header className="flex items-center gap-8 justify-between">
-      <div className="flex items-center gap-2"></div>
+    <header className="flex items-center gap-8 justify-between p-6">
+      {/* Search Area */}
+      <div className="flex items-center gap-2 p-2 flex-grow">
+        <Image
+          src="/icons/search.svg"
+          alt="Search Icon"
+          width={24}
+          height={24}
+        />
+
+        <input
+          type="search"
+          placeholder="Search"
+          className="text-sm font-medium outline-0 flex-grow placeholder:text-[#858D9D] "
+        />
+      </div>
     </header>
   );
 }
